cff08298791ea92135c773186998369c1f61c1bb
Paul Mourraille <paul@pauljmphoto.com>
2019-01-09 18:49:25 -0800

    fixes againnnnnn

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
             <p className="Header__user">Hello {this.props.user.user.username}</p>
           </li>
           <li className="Header__nav-link">
-            <NavLink to="/home/feed">
+            <NavLink to="/exchangagram-frontend/home/feed">
               <i className="fa fa-compass Header__nav-icon" aria-hidden="true" />
             </NavLink>
           </li>
@@ -25,7 +25,7 @@ class Header extends Component {
             </a>
           </li>
           <li className="Header__nav-link">
-            <NavLink to={`/home/profile/${this.props.user.user.id}`}>
+            <NavLink to={`/exchangagram-frontend/home/profile/${this.props.user.user.id}`}>
               <i className="fa fa-smile-o Header__nav-icon" aria-hidden="true" />
             </NavLink>
           </li>
@@ -34,10 +34,10 @@ class Header extends Component {
       links =
         <ul className="Header__nav-group">
           <li className="Header__nav-link">
-            <NavLink to="/">Sign In</NavLink>
+            <NavLink to="/exchangagram-frontend/">Sign In</NavLink>
           </li>
           <li className="Header__nav-link">
-            <NavLink to="/signup">Sign Up</NavLink>
+            <NavLink to="/exchangagram-frontend/signup">Sign Up</NavLink>
           </li>
         </ul>
     }
