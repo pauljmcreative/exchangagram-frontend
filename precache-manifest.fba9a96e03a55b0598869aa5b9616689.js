3d541b264e126e10861a255d43ac9e0d45f552d6
Paul Mourraille <paul@pauljmphoto.com>
2019-01-31 14:49:20 -0800

    Updates

---
+++ precache-manifest.fba9a96e03a55b0598869aa5b9616689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4f0276934e22122f459",
+    "url": "/exchangagram-frontend/static/js/runtime~main.d4f02769.js"
+  },
+  {
+    "revision": "b19c4c777915b8c7ccc3",
+    "url": "/exchangagram-frontend/static/js/main.b19c4c77.chunk.js"
+  },
+  {
+    "revision": "cb7077a6c9f83231acec",
+    "url": "/exchangagram-frontend/static/js/1.cb7077a6.chunk.js"
+  },
+  {
+    "revision": "b19c4c777915b8c7ccc3",
+    "url": "/exchangagram-frontend/static/css/main.7dbf1b13.chunk.css"
+  },
+  {
+    "revision": "5b9b632da11b5b3983a1591df08021e9",
+    "url": "/exchangagram-frontend/index.html"
+  }
+];
